39f264ab66539a89ea8f3c847d0527718dcf84da
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-05-04 16:24:08 -0300

    versao 2.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/SideBar';
 import Header from './Components/Header';
@@ -14,12 +14,24 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // Verifica se o usuário está logado ao carregar a página
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    if (storedLoginStatus === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
+    // Armazena o estado de login no LocalStorage ao fazer login
+    localStorage.setItem('isLoggedIn', true);
   };
 
   const handleLogoutSuccess = () => {
     setIsLoggedIn(false);
+    // Remove o estado de login do LocalStorage ao fazer logout
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
@@ -30,17 +42,16 @@ function App() {
           path="*"
           element={
             isLoggedIn ? (
-                <div style={{ display: 'flex', flexGrow: 1 }}>
-                  <Sidebar onLogout={handleLogoutSuccess}/>
-                  <Routes>
-                    
-                    <Route path="/" element={<Home />} />
-                    <Route path="/ProductPage" element={<ProductsPage />} />
-                    <Route path="/CaixaPage" element={<CaixaPage />} />
-                    <Route path="/Vendas" element={<Vendas />} />
-                    <Route path="/Clientes" element={<Clientes />} />
-                  </Routes>
-                </div>
+              <div style={{ display: 'flex', flexGrow: 1 }}>
+                <Sidebar onLogout={handleLogoutSuccess}/>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/ProductPage" element={<ProductsPage />} />
+                  <Route path="/CaixaPage" element={<CaixaPage />} />
+                  <Route path="/Vendas" element={<Vendas />} />
+                  <Route path="/Clientes" element={<Clientes />} />
+                </Routes>
+              </div>
             ) : (
               <Login onLoginSuccess={handleLoginSuccess} />
             )
