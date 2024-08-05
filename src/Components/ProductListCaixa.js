c5523745f6dd93326d9c9d52803d39e1cceac7f6
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-08-05 15:46:08 -0300

    versao 1.0

--- src/Components/ProductListCaixa.js
+++ src/Components/ProductListCaixa.js
@@ -240,8 +240,8 @@ const handleChange = (e) => {
             {carrinho.map((item) => (
               <div key={item.produto.productid}>
                 <ListItem>
-                  <ListItemText secondaryTypographyProps={{ style: { color: '#c0844a' } }} primaryTypographyProps={{ style: { color: '#c0844a' } }} primary={item.produto.nome} secondary={`Quantidade: ${item.quantidade}`} />
-                  <ListItemText primaryTypographyProps={{ style: { color: '#c0844a' } }} primary={`Total: R$ ${parseFloat(item.produto.precovenda) * item.Quantidade}`} />
+                  <ListItemText secondaryTypographyProps={{ style: { color: '#c0844a' } }} primaryTypographyProps={{ style: { color: '#c0844a' } }} primary={item.nome} secondary={`Quantidade: ${item.quantidade}`} />
+                  <ListItemText primaryTypographyProps={{ style: { color: '#c0844a' } }} primary={`Total: R$ ${parseFloat(item.precovenda) * item.Quantidade}`} />
                   <IconButton onClick={() => removerDoCarrinho(item.produto.productid)} edge="end" aria-label="remover">
                     <DeleteIcon style={{ color: '#c0844a' }}/>
                   </IconButton>
