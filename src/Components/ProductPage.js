27d8aded810ed87d46a97a5bc18667b857d71312
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-08-05 13:27:19 -0300

    versao 1.0

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -12,12 +12,12 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Table from '@mui/material/Table';
-import TableBody from '@mui/material.TableBody';
-import TableCell from '@mui/material.TableCell';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui.material/TableContainer';
-import TableHead from '@mui.material.TableHead';
-import TableRow from '@mui.material.TableRow';
-import Paper from '@mui.material.Paper';
+import TableHead from '@mui.material/TableHead';
+import TableRow from '@mui.material/TableRow';
+import Paper from '@mui.material/Paper';
 import { Grid, Typography } from '@mui.material';
 
 function ProductsPage() {
