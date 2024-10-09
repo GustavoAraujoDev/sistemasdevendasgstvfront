790723397fe8468a62b6b1feaf0da9e9455502e4
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-10-09 16:08:16 -0300

    versao 1.0

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -3,15 +3,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyANnT57uQ4lH8bvhxOctMkgM3D9Kr4Ev9Q",
-    authDomain: "app-100-6f705.firebaseapp.com",
-    databaseURL: "https://app-100-6f705-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "app-100-6f705",
-    storageBucket: "app-100-6f705.appspot.com",
-    messagingSenderId: "883005554199",
-    appId: "1:883005554199:web:68c42d5f89a69f496ea652"
+    apiKey: "AIzaSyAXDfdRG9H2rK2mEnCUaXLMVuHObWfvCsE",
+    authDomain: "testes-19dc4.firebaseapp.com",
+    projectId: "testes-19dc4",
+    storageBucket: "testes-19dc4.appspot.com",
+    messagingSenderId: "400866154921",
+    appId: "1:400866154921:web:ba0e427bf2243030334cb8"
 };
 
 const app = initializeApp(firebaseConfig);
