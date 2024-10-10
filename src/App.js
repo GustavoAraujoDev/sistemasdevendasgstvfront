98a75a4c4149af12155553542bc4a397ce9c97bb
gustavoaraujodev <gustavobarrosaraujo10@gmail.com>
2024-10-10 13:10:41 -0300

    versao 1.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/SideBar';
 import ProductsPage from './Components/ProductPage';
 import Home from './Components/Home';
@@ -7,41 +7,18 @@ import CaixaPage from './Components/ProductListCaixa';
 import Vendas from './Components/Vendas';
 import Clientes from './Components/Clientes';
 import Login from './Components/Login';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedLoginStatus = localStorage.getItem('isLoggedIn');
-    if (storedLoginStatus === 'true') {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-    localStorage.setItem('isLoggedIn', 'true');
-  };
-
-  const handleLogoutSuccess = () => {
-    setIsLoggedIn(false);
-    localStorage.removeItem('isLoggedIn');
-  };
-
   return (
     <Router>
-      <ToastContainer />
-      {isLoggedIn && <Sidebar onLogout={handleLogoutSuccess} />}
+      <Sidebar /> {/* Exiba o Sidebar sem condições de login */}
       <Routes>
-        <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
-        {/* Comente as rotas a seguir para testar apenas o login */}
-        {/* <Route path="/ProductPage" element={<ProductsPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/ProductPage" element={<ProductsPage />} />
         <Route path="/CaixaPage" element={<CaixaPage />} />
         <Route path="/Vendas" element={<Vendas />} />
-        <Route path="/Clientes" element={<Clientes />} /> */}
+        <Route path="/Clientes" element={<Clientes />} />
       </Routes>
     </Router>
   );
